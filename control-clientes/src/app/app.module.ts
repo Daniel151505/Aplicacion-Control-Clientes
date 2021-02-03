25b5d97f8c605e7e81b11dfa8ee37467b3862f68
Daniel151505 <daniel150520011@gmail.com>
2021-02-02 19:06:17 -0500

    Creando Componente Editar-Cliente

--- control-clientes/src/app/app.module.ts
+++ control-clientes/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
 import { TableroComponent } from './componentes/tablero/tablero.component';
 import { ClientesComponent } from './componentes/clientes/clientes.component';
+import { EditarClienteComponent } from './componentes/editar-cliente/editar-cliente.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CabeceroComponent,
     TableroComponent,
-    ClientesComponent
+    ClientesComponent,
+    EditarClienteComponent
   ],
   imports: [
     BrowserModule,
