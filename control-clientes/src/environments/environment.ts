dcb07ecf04f132440c2e0458fb589d5a6576a5f6
Daniel151505 <daniel150520011@gmail.com>
2021-02-03 09:16:16 -0500

    Configurando Archivo Environment

--- control-clientes/src/environments/environment.ts
+++ control-clientes/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firestore:{
+    apiKey: "AIzaSyBa0bQdscwisv78zzktZbU81lCqd7KM5B4",
+    authDomain: "control-clientes-1.firebaseapp.com",
+    databaseURL: "https://control-clientes-1-default-rtdb.firebaseio.com",
+    projectId: "control-clientes-1",
+    storageBucket: "control-clientes-1.appspot.com",
+    messagingSenderId: "628862734136",
+    appId: "1:628862734136:web:3828b4304574bf51b7bbcd",
+    measurementId: "G-G88C9WJFCL"
+  }
 };
 
 /*
