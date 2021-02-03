60b7dee17bdc732fbca5d0b3381385db503b5dc6
Daniel151505 <daniel150520011@gmail.com>
2021-02-03 10:29:16 -0500

    Importando Modulos

--- control-clientes/src/app/app.module.ts
+++ control-clientes/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { environment} from '../environments/environment';
+import { AngularFireModule} from '@angular/fire'
+import { AngularFirestoreModule} from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth'
+import { FlashMessagesModule } from 'angular2-flash-messages'
+import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceroComponent } from './componentes/cabecero/cabecero.component';
@@ -12,6 +17,7 @@ import { RegistroComponent } from './componentes/registro/registro.component';
 import { ConfiguracionComponent } from './componentes/configuracion/configuracion.component';
 import { NoEncontradoComponent } from './componentes/no-encontrado/no-encontrado.component';
 import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.component';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -28,7 +34,12 @@ import { PiePaginaComponent } from './componentes/pie-pagina/pie-pagina.componen
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firestore, 'control-clientes' ),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    FormsModule,
+    FlashMessagesModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
