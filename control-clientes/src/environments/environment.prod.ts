e23bde1a889c48fecef95166591f01d8d2c3e5ba
Daniel151505 <daniel150520011@gmail.com>
2021-02-03 09:17:34 -0500

    Configurando Enviroment Prod

--- control-clientes/src/environments/environment.prod.ts
+++ control-clientes/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyBa0bQdscwisv78zzktZbU81lCqd7KM5B4",
+    authDomain: "control-clientes-1.firebaseapp.com",
+    databaseURL: "https://control-clientes-1-default-rtdb.firebaseio.com",
+    projectId: "control-clientes-1",
+    storageBucket: "control-clientes-1.appspot.com",
+    messagingSenderId: "628862734136",
+    appId: "1:628862734136:web:3828b4304574bf51b7bbcd",
+    measurementId: "G-G88C9WJFCL"
+  }
 };
