c61259c7fa998e7fc299c2a4c95fa693d8ab8976
Daniel151505 <daniel150520011@gmail.com>
2021-02-05 15:26:56 -0500

    Agregando metodo Login

--- control-clientes/src/app/servicios/login.service.ts
+++ control-clientes/src/app/servicios/login.service.ts
@@ -7,5 +7,13 @@ import { AngularFireAuth } from '@angular/fire/auth';
 export class LoginService{
     constructor(private authService: AngularFireAuth){}
 
+    login(email: string, password: string){
+        return new Promise((resolve, reject) => {
+            this.authService.signInWithEmailAndPassword(email, password)
+            .then(datos => resolve(datos),
+                error => reject(error)
+            )
+        })
+    }
 
 }
