e07ba7da977e7d0fc49af29914ceba8000aa4e2c
Daniel151505 <daniel150520011@gmail.com>
2021-02-05 23:58:01 -0500

    Definiendo Variables

--- control-clientes/src/app/componentes/cabecero/cabecero.component.ts
+++ control-clientes/src/app/componentes/cabecero/cabecero.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceroComponent implements OnInit {
 
+  isLoggedIn: boolean;
+  loggedInUser: string;
+
   constructor() { }
 
   ngOnInit(): void {
