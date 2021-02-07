7dcf90b498049a3a68aafe5fe72c4ca5a58224ea
Daniel151505 <daniel150520011@gmail.com>
2021-02-06 23:48:14 -0500

    Importando Map

--- control-clientes/src/app/guardianes/auth.guard.ts
+++ control-clientes/src/app/guardianes/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
 import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router'
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
