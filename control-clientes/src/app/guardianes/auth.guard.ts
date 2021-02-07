25c06a0047d4232a12bbf298b1b38624a140047a
Daniel151505 <daniel150520011@gmail.com>
2021-02-06 23:43:52 -0500

    Importando Servicio

--- control-clientes/src/app/guardianes/auth.guard.ts
+++ control-clientes/src/app/guardianes/auth.guard.ts
@@ -1,5 +1,18 @@
-export class AuthGuard {
-    constructor() {
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router'
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private afAuth: AngularFireAuth
+    ) {
         
     }
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+        throw new Error('Method not implemented.');
+    }
+   
 }
