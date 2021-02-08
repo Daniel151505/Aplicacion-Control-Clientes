ee072d515395a41c901416294c80c11b64a49d44
Daniel151505 <daniel150520011@gmail.com>
2021-02-07 23:51:55 -0500

    Importando Servicio

--- control-clientes/src/app/componentes/configuracion/configuracion.component.ts
+++ control-clientes/src/app/componentes/configuracion/configuracion.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
@@ -9,7 +10,7 @@ export class ConfiguracionComponent implements OnInit {
 
   permitirRegistro = false;
 
-  constructor() { }
+  constructor(private router: Router ) { }
 
   ngOnInit(): void {
   }
